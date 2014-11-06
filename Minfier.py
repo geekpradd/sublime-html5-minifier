c50e58352962a825c6651a365acf798905d22639
geekpradd <pradd@outlook.com>
2014-11-06 20:57:34 +0530

    Offline Functions and Context Menu Added

--- Minfier.py
+++ Minfier.py
@@ -1,7 +1,8 @@
 import sublime, sublime_plugin, urllib.request,urllib.parse,os,sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "modules"))
 from HTMLMinifier import minify
-
+from jsmin import jsmin
+import cssmin
 
 
 
@@ -21,11 +22,8 @@ class MinifierCommand(sublime_plugin.TextCommand):
 
     if  self.extension == 'js':
 
-      data={'js_code':content,'compilation_level':'WHITESPACE_ONLY','output_format':'text','output_info':'compiled_code'}
-      d=urllib.parse.urlencode(data)
-      d=d.encode('utf-8')
-      req=urllib.request.urlopen('http://closure-compiler.appspot.com/compile',d)
-      code=str(req.read())[2:-3:]
+
+      code=jsmin(content)
 
       file=open(self.path,'w')
       file.write(code)
@@ -39,11 +37,8 @@ class MinifierCommand(sublime_plugin.TextCommand):
       file.close()
       self.view.window().open_file(self.path)
     elif self.extension=='css':
-      data={'input':content}
-      d=urllib.parse.urlencode(data)
-      d=d.encode('utf-8')
-      req=urllib.request.urlopen('http://cssminifier.com/raw',d)
-      code=str(req.read())[2:-1:]
+
+      code=cssmin.cssmin(content)
       file=open(self.path,'w')
       file.write(code)
       file.close()
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #Sublime HTML5 Minifier
 
-This is a Sublime Text 3 Plugin for reducing the code size of HTML5, CSS3 and Javascript files. This plugin requires to be connected to the Internet to work.
+This is a Sublime Text 3 Plugin for reducing the code size of HTML5, CSS3 and Javascript files. 
 
 ##Installation
 
@@ -11,6 +11,8 @@ You can install Package Control by following these steps: [https://sublime.wbond
 ##Usage
 
 In order to minify code, Click on the HTML5 Minifier option and click Minify Current File. A .min file be added onto the current directory where you are working. 
+
+Alternatively, Right Click on the Editor area or the sidebar and click 'Minify HTML5 File'.
 
 For example, if you are working on a file 'main.css' in the location 'C:\Projects\' then a minified file will be created at 'C:\Projects\' with the name main.min.css making the full path 'C:\Projects\main.min.css'.
 
